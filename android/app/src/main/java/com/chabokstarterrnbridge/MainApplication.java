296736a33fdf6f498bc0aefc2aeba98b30662aa1
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2018-09-16 18:52:17 +0430

    Initialize the Chabok class in Application class

--- android/app/src/main/java/com/chabokstarterrnbridge/MainApplication.java
+++ android/app/src/main/java/com/chabokstarterrnbridge/MainApplication.java
@@ -2,7 +2,9 @@ package com.chabokstarterrnbridge;
 
 import android.app.Application;
 
+import com.adpdigital.push.AdpPushClient;
 import com.facebook.react.ReactApplication;
+import com.adpdigital.push.rn.ChabokReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -12,34 +14,54 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+    private AdpPushClient chabok = null;
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new ChabokReactPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        if (chabok == null) {
+            chabok = AdpPushClient.init(
+                    getApplicationContext(),
+                    MainActivity.class,
+                    "chabok-starter/339811759516",
+                    "70df4ae2e1fd03518ce3e3b21ee7ca7943577749",
+                    "chabok-starter",
+                    "chabok-starter"
+            );
+        }
     }
 
     @Override
-    protected String getJSMainModuleName() {
-      return "index";
+    public void onTerminate() {
+        if (chabok != null)
+            chabok.dismiss();
+
+        super.onTerminate();
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
