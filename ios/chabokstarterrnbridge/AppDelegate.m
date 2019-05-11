c51bd5e0e52e3979f552fb37f29dcb2777b69aa3
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-05-11 16:54:02 +0430

    Add RCTLinkingManager to iOS

--- ios/chabokstarterrnbridge/AppDelegate.m
+++ ios/chabokstarterrnbridge/AppDelegate.m
@@ -7,6 +7,7 @@
 
 #import "AppDelegate.h"
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 #import <React/RCTBundleURLProvider.h>
 #import <AdpPushClient/AdpPushClient.h>
 
@@ -48,4 +49,22 @@
   [PushClientManager.defaultManager application:application didRegisterUserNotificationSettings:notificationSettings];
 }
   
-  @end
+#pragma mark - deep link
+  
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+  {
+    return [RCTLinkingManager application:application openURL:url
+                        sourceApplication:sourceApplication annotation:annotation];
+  }
+  
+  // Only if your app is using [Universal Links](https://developer.apple.com/library/prerelease/ios/documentation/General/Conceptual/AppSearch/UniversalLinks.html).
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
+  {
+    return [RCTLinkingManager application:application
+                     continueUserActivity:userActivity
+                       restorationHandler:restorationHandler];
+  }
+  
+@end
